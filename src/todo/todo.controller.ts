0454d51d71a8cdcb785e683700f0a2bc08d46560
Alejandro Estrada <alejandro@MacBook-Pro-de-Alejandro.local>
2023-03-18 01:23:56 -0500

    finalizacion de crud basico

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
@@ -10,6 +10,26 @@ export class TodoController {
   @Get()
   findAll() {
     return this.todoService.findAll();
+  }
+
+  @Get(':id')
+  findOneById(@Param('id', ParseIntPipe) id: number) {
+    return this.todoService.findOneById(id);
+  }
+
+  @Post()
+  createTodo(@Body() createTodoDto: CreateTodoDto){
+    return this.todoService.create(createTodoDto);
+  }
+
+  @Patch(':id')
+  updateTodo(@Param('id', ParseIntPipe)id: number, @Body() updateTodoDto: UpdateTodoDto){
+    return this.todoService.updateTodo(id, updateTodoDto);
+  }
+
+  @Delete(':id')
+  deleteTodo(@Param('id', ParseIntPipe) id: number){
+    return this.todoService.deleteTodo(id);
   }
 
 }
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,10 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from './entities/todo.entity';
 
 @Injectable()
 export class TodoService {
-  findAll() {
-    return `This action returns all todo`;
+
+  private todos: Todo[] = [
+    {
+      id: 0,
+      description: "Piedra del Alma",
+      done: false,
+    },
+    {
+      id: 1,
+      description: "Piedra del Tiempo",
+      done: false,
+    },
+    {
+      id: 2,
+      description: "Piedra del Espacio",
+      done: false,
+    },
+  ];
+
+  create({description}: CreateTodoDto): Todo{
+    const todo = new Todo();
+    
+    todo.description = description;
+    todo.id = Math.max(...this.todos.map(todo => todo.id), 0) + 1;
+
+    this.todos = [...this.todos, todo];
+
+    return todo;
+  }
+
+  findAll(): Todo[] {
+    return this.todos;
+  }
+
+  findOneById(id: number): Todo{
+    const todoInstance: Todo = this.todos.find(todo => todo.id === id);
+
+    if(todoInstance)
+      return todoInstance;
+
+    throw new NotFoundException(`todo with id ${id} not found`);
+  }
+
+  updateTodo(id: number, {description, done}: UpdateTodoDto): Todo{
+
+    const todoToUpdate = this.findOneById(id);
+
+    if(description) todoToUpdate.description = description;
+    if(done) todoToUpdate.done = done;
+
+    this.todos.forEach(todo => {
+      if(todo.id === id){
+        todo = {
+          ...todo,
+          ...todoToUpdate
+        }
+      }
+    });
+    return todoToUpdate;
+  }
+
+  deleteTodo(id: number){
+
+    const todo = this.findOneById(id);
+
+    this.todos = this.todos.filter(todo => todo.id !== id);
+
+    return todo;
   }
 }
