5bc3058a17ba701f5d81bae66654dec11e4824d8
Alejandro Estrada <alejandro@MacBook-Pro-de-Alejandro.local>
2023-03-18 15:32:48 -0500

    creacion base del modulo del resolver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 import { TodoModule } from './todo/todo.module';
+import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { HelloWorldModule } from './hello-world/hello-world.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    TodoModule, 
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // playground: false, //Para deshabilitar la interfaz grafica
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      // plugins: [
+      //   ApolloServerPluginLandingPageLocalDefault()
+      // ]
+    }), HelloWorldModule,
+  ],
   controllers: [],
   providers: [],
 })
