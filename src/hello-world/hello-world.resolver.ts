356741cc85c22a8f1db66a9fc129537d267f93ec
Alejandro Estrada <alejoestradam@yahoo.com>
2023-03-18 16:26:20 -0500

    we use @Args() for queries

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,10 +1,19 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import { skipUntil } from 'rxjs';
 
 @Resolver()
 export class HelloWorldResolver {
 
-    @Query(() => String)
+    @Query(() => String, {name: 'Hello', description: 'Hello world which returns "Hello World" message'})
     helloWorld(): string {
         return "Hola Mundo"
     }
+
+    @Query(() => Int, {name: 'Random', description: 'Random number from 0 to specified number'})
+    randomNumbert(@Args('to', {nullable: true ,type: () => Int}) tillNumber: number  = 6): number{
+        //* 'to' on @Args() is the one exposed on apollo to wotk with
+        return Math.floor(Math.random() * tillNumber);
+    }
+
+
 }
