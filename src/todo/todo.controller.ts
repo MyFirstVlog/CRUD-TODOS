b21275a4734e744750a4339e4c52cf599968fa07
Alejandro Estrada <alejandro@MacBook-Pro-de-Alejandro.local>
2023-03-18 00:45:28 -0500

    se borran metodos inncesarios, se definen los dtos, entities

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -7,28 +7,9 @@ import { UpdateTodoDto } from './dto/update-todo.dto';
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
-  @Post()
-  create(@Body() createTodoDto: CreateTodoDto) {
-    return this.todoService.create(createTodoDto);
-  }
-
   @Get()
   findAll() {
     return this.todoService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.todoService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
-    return this.todoService.update(+id, updateTodoDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.todoService.remove(+id);
-  }
 }
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -4,23 +4,7 @@ import { UpdateTodoDto } from './dto/update-todo.dto';
 
 @Injectable()
 export class TodoService {
-  create(createTodoDto: CreateTodoDto) {
-    return 'This action adds a new todo';
-  }
-
   findAll() {
     return `This action returns all todo`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} todo`;
-  }
-
-  update(id: number, updateTodoDto: UpdateTodoDto) {
-    return `This action updates a #${id} todo`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} todo`;
   }
 }
